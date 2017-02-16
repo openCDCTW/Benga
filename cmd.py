f567be2e85dfc7fc11d96830d4d589a01b5d2038
Yueh-Hua Tu <a504082002@gmail.com>
2017-02-16 15:41:20 +0800

    add --local for locally installed dependencies

--- cmd.py
+++ cmd.py
@@ -45,6 +45,13 @@ def parse_args():
         metavar='THRESHOLD'
     )
 
+    arg_parser.add_argument(
+        "--local",
+        help="Use locally installed prokka and roary, instead of the docker version.",
+        action='store_true',
+        default=False
+    )
+
     return arg_parser.parse_args()
 
 
@@ -56,13 +63,14 @@ def main():
     db_dir = args.database
     threads = args.threads
     identity = args.identity
+    docker = not args.local
 
     if args.algorithm == "make_db":
-        pgdb.annotate_configs(input_dir, output_dir)
-        pgdb.make_database(output_dir, threads=threads, min_identity=identity)
+        pgdb.annotate_configs(input_dir, output_dir, use_docker=False)
+        pgdb.make_database(output_dir, threads=threads, min_identity=identity, use_docker=docker)
     if args.algorithm == "profiling":
         wgmlst.profiling(output_dir, input_dir, db_dir,
-                         threads=threads, aligcov_cut=0.5, identity=identity)
+                         threads=threads, aligcov_cut=0.5, identity=identity, use_docker=docker)
 
 
 if __name__ == "__main__":
