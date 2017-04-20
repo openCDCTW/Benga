194f8c7f5b95a7533864f09949d78286218c1a81
Yueh-Hua Tu <a504082002@gmail.com>
2017-04-20 11:03:51 +0800

    Remove identity option from user interface

--- wgmlst-cmd.py
+++ wgmlst-cmd.py
@@ -38,14 +38,6 @@ def parse_args():
     )
 
     arg_parser.add_argument(
-        "--identity",
-        help="The minimum percentage identity for blastp. [Default: 90]",
-        type=int,
-        default=90,
-        metavar='THRESHOLD'
-    )
-
-    arg_parser.add_argument(
         "--local",
         help="Use locally installed prokka and roary, instead of the docker version.",
         action='store_true',
@@ -62,15 +54,13 @@ def main():
     output_dir = args.output
     db_dir = args.database
     threads = args.threads
-    identity = args.identity
     docker = not args.local
 
     if args.algorithm == "make_db":
         pgdb.annotate_configs(input_dir, output_dir, use_docker=docker)
         pgdb.make_database(output_dir, threads=threads, use_docker=docker)
     if args.algorithm == "profiling":
-        wgmlst.profiling(output_dir, input_dir, db_dir,
-                         threads=threads, aligcov_cut=0.5, identity=identity)
+        wgmlst.profiling(output_dir, input_dir, db_dir, threads=threads)
 
 
 if __name__ == "__main__":
