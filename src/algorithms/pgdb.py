99d66e285bbdd462c67ea1734f847b323a73202a
Yueh-Hua Tu <a504082002@gmail.com>
2017-05-03 16:26:53 +0800

    Refactoring file names

--- src/algorithms/pgdb.py
+++ src/algorithms/pgdb.py
@@ -18,11 +18,11 @@ def parse_filenames(path, ext=".fna"):
 def format_contigs(filenames, input_dir, working_dir):
     namemap = {}
     for i, oldname in enumerate(filenames, 1):
-        newname = "Assembly_{}.fa".format(i)
+        newname = "Genome_{}.fa".format(i)
         namemap[oldname] = newname
         with open(files.joinpath(working_dir, newname), "w") as file:
             for j, contig in enumerate(SeqIO.parse(files.joinpath(input_dir, oldname), "fasta"), 1):
-                seqid = "A_{}::C_{}".format(i, j)
+                seqid = "G_{}::C_{}".format(i, j)
                 SeqIO.write(seq.replace_id(contig, seqid), file, "fasta")
     return namemap
 
@@ -67,8 +67,8 @@ def most_frequent_allele(row):
     return counter.most_common(1)[0][0]
 
 
-def identify_pan_refseq(output_dir, ffn_dir, locus_dir):
-    profiles, isolates = extract_profiles(output_dir)
+def identify_pan_refseq(output_dir, ffn_dir, locus_dir, locusmeta_file, paralogmeta_file, profile_file):
+    profiles, isolates = extract_profiles(output_dir, locusmeta_file, paralogmeta_file)
     new_profiles = pd.DataFrame(columns=profiles.columns, index=profiles.index)
     allele_map = defaultdict(set)
     for colname, col in profiles.iteritems():
@@ -79,7 +79,7 @@ def identify_pan_refseq(output_dir, ffn_dir, locus_dir):
                 s = str(cds[allele])
                 allele_map[locus].add(s)
                 new_profiles.loc[locus, colname] = operations.make_seqid(s)
-    new_profiles.to_csv(files.joinpath(output_dir, "allele_profiles.tsv"), sep="\t")
+    new_profiles.to_csv(profile_file, sep="\t")
 
     frequent = {}
     for locus, alleles in allele_map.items():
@@ -88,35 +88,35 @@ def identify_pan_refseq(output_dir, ffn_dir, locus_dir):
     return frequent, isolates
 
 
-def extract_profiles(output_dir, metadata_colnumber=14):
+def extract_profiles(output_dir, locusmeta_file, paralogmeta_file, metadata_colnumber=14):
     profile_matrix = pd.read_csv(files.joinpath(output_dir, "roary", "gene_presence_absence.csv"))
     profile_matrix.index = pd.Index(map(lambda x: "SAL{0:07d}".format(x + 1), profile_matrix.index), name="locus")
     isolates = len(profile_matrix.columns) - metadata_colnumber
-    appear_once_locus = divide_matrix(profile_matrix, output_dir)
+    appear_once_locus = divide_matrix(profile_matrix, locusmeta_file, paralogmeta_file)
     profiles = appear_once_locus.iloc[:, metadata_colnumber:]
     return profiles, isolates
 
 
-def divide_matrix(mat, output_dir):
+def divide_matrix(mat, locusmeta_file, paralogmeta_file):
     appear_once_locus = mat[mat["No. isolates"] == mat["No. sequences"]]
     not_appear_once_locus = mat[mat["No. isolates"] != mat["No. sequences"]]
-    save_metadata(appear_once_locus, not_appear_once_locus, output_dir)
+    save_metadata(appear_once_locus, not_appear_once_locus, locusmeta_file, paralogmeta_file)
     return appear_once_locus
 
 
-def save_metadata(appear_once_locus, not_appear_once_locus, output_dir):
+def save_metadata(appear_once_locus, not_appear_once_locus, locusmeta_file, paralogmeta_file):
     select_col = ["Gene", "No. isolates", "No. sequences", "Annotation"]
     metadata = appear_once_locus[select_col]
     paralog = not_appear_once_locus[select_col]
-    metadata.to_csv(files.joinpath(output_dir, "locus_metadata.tsv"), sep="\t")
-    paralog.to_csv(files.joinpath(output_dir, "paralog_metadata.tsv"), sep="\t")
+    metadata.to_csv(locusmeta_file, sep="\t")
+    paralog.to_csv(paralogmeta_file, sep="\t")
 
 
-def make_schemes(mapping_file, refseqs, total_isolates, scheme_dir):
-    mapping = pd.read_csv(mapping_file, sep="\t")
+def make_schemes(locusmeta_file, scheme_file, refseqs, total_isolates):
+    mapping = pd.read_csv(locusmeta_file, sep="\t")
     mapping["occurence"] = list(map(lambda x: round(x/total_isolates * 100, 2), mapping["No. isolates"]))
     mapping["sequence"] = list(map(lambda x: refseqs[x], mapping["locus"]))
-    mapping[["locus", "occurence", "sequence"]].to_csv(files.joinpath(scheme_dir, "scheme.csv"), index=False)
+    mapping[["locus", "occurence", "sequence"]].to_csv(scheme_file, index=False, sep="\t")
 
 
 def annotate_configs(input_dir, output_dir, logger=None, use_docker=True):
@@ -126,9 +126,9 @@ def annotate_configs(input_dir, output_dir, logger=None, use_docker=True):
     logger.info("Formating contigs...")
     filenames = parse_filenames(input_dir)
 
-    assembly_dir = files.joinpath(output_dir, "Assembly")
-    files.create_if_not_exist(assembly_dir)
-    namemap = format_contigs(filenames, input_dir, assembly_dir)
+    genome_dir = files.joinpath(output_dir, "Genomes")
+    files.create_if_not_exist(genome_dir)
+    namemap = format_contigs(filenames, input_dir, genome_dir)
     with open(files.joinpath(output_dir, "namemap.json"), "w") as f:
         f.write(json.dumps(namemap))
 
@@ -136,9 +136,9 @@ def annotate_configs(input_dir, output_dir, logger=None, use_docker=True):
     annotate_dir = files.joinpath(output_dir, "Annotated")
     files.create_if_not_exist(annotate_dir)
     if use_docker:
-        docker.prokka(assembly_dir, annotate_dir)
+        docker.prokka(genome_dir, annotate_dir)
     else:
-        c = [cmds.form_prokka_cmd(x, assembly_dir, annotate_dir) for x in namemap.values()]
+        c = [cmds.form_prokka_cmd(x, genome_dir, annotate_dir) for x in namemap.values()]
         with ProcessPoolExecutor() as executor:
             executor.map(os.system, c)
 
@@ -160,7 +160,7 @@ def make_database(output_dir, logger=None, threads=2, use_docker=True):
     if not logger:
         logger = logs.console_logger(__name__)
 
-    database_dir = files.joinpath(output_dir, "DB")
+    database_dir = files.joinpath(output_dir, "database")
     files.create_if_not_exist(database_dir)
 
     logger.info("Calculating the pan genome...")
@@ -173,22 +173,20 @@ def make_database(output_dir, logger=None, threads=2, use_docker=True):
 
     logger.info("Finding most frequent allele as RefSeq...")
     ffn_dir = files.joinpath(output_dir, "FFN")
-    locus_dir = files.joinpath(output_dir, "locusfiles")
+    locus_dir = files.joinpath(database_dir, "locusfiles")
     files.create_if_not_exist(locus_dir)
-    refseqs, total_isolates = identify_pan_refseq(output_dir, ffn_dir, locus_dir)
+    locusmeta_file = files.joinpath(database_dir, "locus_metadata.tsv")
+    paralogmeta_file = files.joinpath(database_dir, "paralog_metadata.tsv")
+    profile_file = files.joinpath(database_dir, "allele_profiles.tsv")
+    refseqs, total_isolates = identify_pan_refseq(output_dir, ffn_dir, locus_dir,
+                                                  locusmeta_file, paralogmeta_file, profile_file)
 
     logger.info("Saving pan RefSeq...")
     records = [seq.new_record(key, str(value)) for key, value in refseqs.items()]
     SeqIO.write(records, files.joinpath(database_dir, "panRefSeq.fa"), "fasta")
 
     logger.info("Making dynamic schemes...")
-    mapping_file = files.joinpath(output_dir, "locus_metadata.tsv")
-    make_schemes(mapping_file, refseqs, total_isolates, database_dir)
-
-    logger.info("Collecting outputs...")
-    shutil.move(files.joinpath(output_dir, "locus_metadata.tsv"), database_dir)
-    shutil.move(files.joinpath(output_dir, "paralog_metadata.tsv"), database_dir)
-    shutil.move(files.joinpath(output_dir, "allele_profiles.tsv"), database_dir)
-    shutil.move(locus_dir, database_dir)
+    scheme_file = files.joinpath(database_dir, "scheme.tsv")
+    make_schemes(locusmeta_file, scheme_file, refseqs, total_isolates)
     logger.info("Done!!")
 
