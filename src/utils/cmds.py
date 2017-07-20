ea42a2365bb5f031b45bd51236562636f6ab3d60
Yueh-Hua Tu <a504082002@gmail.com>
2017-07-20 14:08:06 +0800

    Let roary generate reference sequence for each gene/cluster

--- src/utils/cmds.py
+++ src/utils/cmds.py
@@ -14,6 +14,7 @@ def form_prokka_cmd(newname, inpath, outpath):
 def form_roary_cmd(inpath, outpath, ident_min, threads):
     args = list()
     args.append(("-p", threads))
+    args.append(("-e", ""))  # generate pan_genome_reference.fa for reference sequence of gene/cluster
     args.append(("-i", ident_min))
     args.append(("-f", files.joinpath(outpath, "roary")))
     return operations.format_cmd("roary", args, files.joinpath(inpath, "*.gff"))
