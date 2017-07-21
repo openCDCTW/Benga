eeb848cb0e3506425390582a2988285cea1e530b
Yueh-Hua Tu <a504082002@gmail.com>
2017-07-21 11:29:50 +0800

    Let roary generate multifasta files of each gene

--- src/utils/cmds.py
+++ src/utils/cmds.py
@@ -15,6 +15,7 @@ def form_roary_cmd(inpath, outpath, ident_min, threads):
     args = list()
     args.append(("-p", threads))
     args.append(("-e", ""))  # generate pan_genome_reference.fa for reference sequence of gene/cluster
+    args.append(("--dont_delete_files", ""))  # multifasta files of each gene
     args.append(("-i", ident_min))
     args.append(("-f", files.joinpath(outpath, "roary")))
     return operations.format_cmd("roary", args, files.joinpath(inpath, "*.gff"))
