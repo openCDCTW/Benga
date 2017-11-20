215f2d2b7676a703195c935292330fe3148b441c
Yueh-Hua Tu <a504082002@gmail.com>
2017-11-20 09:57:30 +0800

    Remove pararmeters from roary commands

--- src/utils/cmds.py
+++ src/utils/cmds.py
@@ -15,8 +15,7 @@ def form_prokka_cmd(newname, inpath, outpath):
 def form_roary_cmd(inpath, outpath, ident_min, threads):
     args = list()
     args.append(("-p", threads))
-    args.append(("-s", ""))  # don't split paralogs
-    args.append(("-e", ""))  # generate pan_genome_reference.fa for reference sequence of gene/cluster
+    args.append(("--mafft", ""))
     args.append(("-i", ident_min))
     args.append(("-f", files.joinpath(outpath, "roary")))
     return operations.format_cmd("roary", args, files.joinpath(inpath, "*.gff"))
