6fe6f3a8862bdfa1aeacb430d942a9ac430f00f5
Yueh-Hua Tu <a504082002@gmail.com>
2017-12-18 15:18:31 +0800

    Add roary parameter '-s'

--- src/utils/cmds.py
+++ src/utils/cmds.py
@@ -14,8 +14,8 @@ def form_prokka_cmd(newname, inpath, outpath):
 
 def form_roary_cmd(inpath, outpath, ident_min, threads):
     args = list()
+    args.append(("-s", ""))
     args.append(("-p", threads))
-    args.append(("--mafft", ""))
     args.append(("-i", ident_min))
     args.append(("-f", files.joinpath(outpath, "roary")))
     return operations.format_cmd("roary", args, files.joinpath(inpath, "*.gff"))
