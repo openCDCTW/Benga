ca2ec4062db4d839fbcc86783542d8aae468e408
Yueh-Hua Tu <a504082002@gmail.com>
2018-01-08 14:05:51 +0800

    Refactor and remove not used functions

--- src/algorithms/wgmlst.py
+++ src/algorithms/wgmlst.py
@@ -1,7 +1,6 @@
 import json
 import os
 import shutil
-import functional
 import pandas as pd
 import functools
 from concurrent.futures import ProcessPoolExecutor
@@ -56,110 +55,6 @@ def rename(query_dir, input_dir):
         SeqIO.write(records, files.joinpath(query_dir, newname), "fasta")
         namemap[files.replace_ext(newname)] = files.replace_ext(filename)
     return namemap
-
-
-def profile_loci(refseq_fna, query_dir, output_dir, aligcov_cut, identity, threads):
-    refseqlen = (functional.seq(SeqIO.parse(refseq_fna, "fasta"))
-                 .map(lambda rec: (rec.id, len(rec.seq)))
-                 .to_dict())
-
-    args = [(x, query_dir, refseq_fna, refseqlen, aligcov_cut, identity)
-            for x in os.listdir(query_dir)]
-    with ProcessPoolExecutor(threads) as executor:
-        collect = {k: v for k, v in executor.map(extract_locus, args)}
-
-    refseqs = list(refseqlen.keys())
-    series = []
-    for cid, loci in collect.items():
-        xs = [s in loci for s in refseqs]
-        ser = pd.Series(xs, name=cid, index=refseqs)
-        series.append(ser)
-    table = pd.concat(series, axis=1).sort_index(axis=0).sort_index(axis=1)
-    table.to_csv(files.joinpath(output_dir, "locus_profiles.tsv"), sep="\t")
-
-
-def extract_locus(args):
-    filename, query_dir, refseq_fna, refseqlen, aligcov_cut, identity = args
-    contig_file = files.joinpath(query_dir, filename)
-    contig_id = filename.split(".")[0]
-
-    db_dir = os.path.join(query_dir, contig_id)
-    blastn_out_file = files.joinpath(query_dir, "{}.out".format(contig_id))
-
-    seq.compile_blastndb(contig_file, db_dir)
-    seq.query_blastndb(refseq_fna, db_dir, blastn_out_file, seq.BLAST_COLUMNS)
-    matched_loci = identify_locus(blastn_out_file, refseqlen, aligcov_cut, identity, seq.BLAST_COLUMNS)
-
-    os.remove(db_dir + ".nhr")
-    os.remove(db_dir + ".nin")
-    os.remove(db_dir + ".nsq")
-    os.remove(blastn_out_file)
-    return contig_id, matched_loci
-
-
-def identify_locus(blast_out, seqlen, aligcov_cut, identity, cols):
-    result = pd.read_csv(blast_out, sep="\t", header=None, names=cols)
-    result["qlen"] = [seqlen[x] for x in result["qseqid"]]
-    result["aligcov"] = (result["length"] - result["gapopen"]) / result["qlen"]
-    result = result[(result["aligcov"] >= aligcov_cut) & (result["pident"] >= identity)]
-    return set(result["qseqid"])
-
-
-def exactly_match_in(records1, records2):
-    for r1 in records1:
-        r1_rev = r1.seq.reverse_complement()
-        for r2 in records2:
-            if r1.seq in r2.seq:
-                return r1.id
-            if r1_rev in r2.seq:
-                return r1.id
-    return None
-
-
-def profile_alleles(query_dir, db_dir, output_dir, threads, occr_level, selector=None):
-    locusfiles = files.joinpath(db_dir, "locusfiles")
-    profiles = select_loci(db_dir, output_dir, occr_level, selector)
-
-    collect = []
-    with ProcessPoolExecutor(threads) as executor:
-        for contig, profile in profiles.iteritems():
-            contig_file = files.joinpath(query_dir, "{}.fa".format(contig))
-            records = list(SeqIO.parse(contig_file, "fasta"))
-            matched = profile[profile]
-
-            args = [(locus, locusfiles, records) for locus in matched.index]
-            series = pd.Series(name=contig)
-            for x in executor.map(match_allele, args):
-                if x:
-                    locus, allele = x
-                    series = series.set_value(locus, allele)
-            collect.append(series)
-    result = pd.concat(collect, axis=1)
-    result.to_csv(files.joinpath(output_dir, "wgmlst.tsv"), sep="\t")
-
-
-def select_loci(db_dir, output_dir, occr_level, selector):
-    profile_file = files.joinpath(output_dir, "locus_profiles.tsv")
-    profiles = pd.read_csv(profile_file, sep="\t", index_col=0)
-    scheme = pd.read_csv(files.joinpath(db_dir, "scheme.tsv"), usecols=[0, 1], sep="\t")
-    if not selector:
-        selected_loci = scheme[scheme["occurrence"] >= occr_level]["locus"]
-    elif type(selector) == list:
-        selected_loci = selector
-    else:
-        selected_loci = scheme["locus"]
-    profiles = profiles[profiles.index.isin(selected_loci)]
-    return profiles
-
-
-def match_allele(args):
-    locus, locusfiles, records = args
-    alleles_file = files.joinpath(locusfiles, "{}.fa".format(locus))
-    alleles = list(SeqIO.parse(alleles_file, "fasta"))
-    matched_allele = exactly_match_in(alleles, records)
-    if matched_allele:
-        return locus, matched_allele
-    return None
 
 
 def make_ref_blastpdb(ref_db_file, database):
--- src/utils/cmds.py
+++ src/utils/cmds.py
@@ -4,19 +4,20 @@ from src.utils import files
 
 def form_prokka_cmd(newname, inpath, outpath):
     name, ext = newname.split(".")
-    return ["prokka", "--prefix", name, "--cpus", "2",
-            "--outdir", files.joinpath(outpath, name),
+    args = ["prokka", "--prefix", name, "--cpus", "2", "--outdir", files.joinpath(outpath, name),
             files.joinpath(inpath, newname)]
+    return " ".join(args)
 
 
 def form_roary_cmd(inpath, outpath, ident_min, threads):
-    return ["roary", "-p", threads, "-i", ident_min, "-s",
-            "-f", files.joinpath(outpath, "roary"),
+    args = ["roary", "-p", threads, "-i", ident_min, "-s", "-f", files.joinpath(outpath, "roary"),
             files.joinpath(inpath, "*.gff")]
+    return " ".join(args)
 
 
 def form_prodigal_cmd(infile, outpath):
     filename = files.fasta_filename(infile)
-    return ["prodigal", "-c", "-m", "-q", "-g", "11", "-i", infile,
+    args = ["prodigal", "-c", "-m", "-q", "-g", "11", "-i", infile,
             "-d", os.path.join(outpath, filename + ".locus.fna"),
             "-o", os.path.join(outpath, filename + ".gbk")]
+    return " ".join(args)
