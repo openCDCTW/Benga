4f7156c5374b3ae76d2aac38d17c5774016c0768
Yueh-Hua Tu <a504082002@gmail.com>
2018-02-08 11:33:08 +0800

    Refactoring

--- src/utils/cmds.py
+++ src/utils/cmds.py
@@ -7,13 +7,13 @@ def form_prokka_cmd(newname, inpath, outpath):
     name, ext = newname.split(".")
     args = ["prokka", "--prefix", name, "--cpus", "2", "--outdir", files.joinpath(outpath, name),
             files.joinpath(inpath, newname)]
-    return " ".join(args)
+    return " ".join(map(str, args))
 
 
 def form_roary_cmd(inpath, outpath, ident_min, threads):
-    args = ["roary", "-p", str(threads), "-i", str(ident_min), "-s", "-f", files.joinpath(outpath, "roary"),
+    args = ["roary", "-p", threads, "-i", ident_min, "-s", "-f", files.joinpath(outpath, "roary"),
             files.joinpath(inpath, "*.gff")]
-    return " ".join(args)
+    return " ".join(map(str, args))
 
 
 def form_prodigal_cmd(infile, outpath):
@@ -21,7 +21,7 @@ def form_prodigal_cmd(infile, outpath):
     args = ["prodigal", "-c", "-m", "-q", "-g", "11", "-i", infile,
             "-d", os.path.join(outpath, filename + ".locus.fna"),
             "-o", os.path.join(outpath, filename + ".gbk")]
-    return " ".join(args)
+    return " ".join(map(str, args))
 
 
 def execute_cmd(args):
