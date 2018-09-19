51e4d4426c34039be749f293a73bac1f67a7a805
Yueh-Hua Tu <a504082002@gmail.com>
2018-09-19 16:29:52 +0800

    Add first test case

--- profiling/tests.py
+++ profiling/tests.py
@@ -1,7 +1,22 @@
-from django.test import TestCase
-import src.algorithms.databases
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
-class SmokingTestCase(TestCase):
-    def test_import(self):
-        pass
+class UploadbatchTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
 
+    def tearDown(self):
+        self.client = None
+
+    def test_get_batch_id(self):
+        """
+        Ensure we can create a new UploadBatch object.
+        """
+        url = reverse("upload-list")
+        data = {}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.keys(), {"id", "created"},
+                         "Recieved object does not contain 'id' and 'created' field.")
