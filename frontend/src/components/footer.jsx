da55cb38d0624f673928c08c5fd19e43ebe45a84
y.s.chen <y.s.chen@cdc.gov.tw>
2018-10-18 16:09:36 +0800

    fix

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Footer extends React.Component {
+
+
+    render() {
+        return (
+		<footer>
+		<div>
+        <font>Copyright (c) 2018 Centrallab, Taiwan (Taiwan CDC).</font>
+        <font> Contact us</font>
+        </div>
+  		</footer>
+        );
+    }
+}
