e41620ee9e449579264cd43ca825b8d540e69bbc
Yueh-Hua Tu <a504082002@gmail.com>
2018-10-24 15:33:03 +0800

    Remove Django channel usage

--- benga/settings.py
+++ benga/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'channels',
     'profiling.apps.ProfilingConfig',
 	'frontend',
     'django_celery_results',
--- profiling/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from profiling.consumers import ProfilingConsumer
-
-websocket_urlpatterns = [
-    path('ws/profiling/<uuid:batch_id>/', ProfilingConsumer),
-]
