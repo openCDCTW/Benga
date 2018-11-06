a387910d98eebd8beefb988be2850828e37d9604
Yueh-Hua Tu <a504082002@gmail.com>
2018-11-06 14:04:34 +0800

    Improve CLI

--- benga.py
+++ benga.py
@@ -1,120 +1,84 @@
-import argparse
+import click
 import datetime
 import os.path
 import subprocess
 from src.algorithms import databases, profiling, phylogeny, statistics
 
 
-def parse_args():
-    arg_parser = argparse.ArgumentParser()
-
-    arg_parser.add_argument(
-        "-a", "--algorithm",
-        required=True,
-        choices=["make_db", "profiling", "tree", "statistics"],
-        help="Execute specified algorithm. (necessary)"
-    )
-
-    arg_parser.add_argument(
-        "-o", "--output",
-        help="Output data directory. (necessary)"
-    )
-
-    arg_parser.add_argument(
-        "-i", "--input",
-        help="Input data (for query) directory. (necessary)"
-    )
-
-    arg_parser.add_argument(
-        "-d", "--database",
-        help="Pan genome allele database for query. (necessary for profiling)"
-    )
-
-    arg_parser.add_argument(
-        "--occr",
-        type=int,
-        default=95,
-        help="Level of occurrence for scheme selection. [Default: 95]",
-        metavar="OCCR"
-    )
-
-    arg_parser.add_argument(
-        "--drop_by_occur",
-        type=float,
-        default=0.0,
-        help="Level of occurrence to drop. (necessary for make_db)",
-        metavar="DROP"
-    )
-
-    arg_parser.add_argument(
-        "-t", "--threads",
-        type=int,
-        default=2,
-        help="Number of threads for computation. [Default: 2]",
-        metavar="THREADS"
-    )
-
-    arg_parser.add_argument(
-        "--no_new_alleles",
-        help="Disable adding new alleles to database. [Default: False]",
-        action='store_true',
-        default=False
-    )
-
-    arg_parser.add_argument(
-        "--no_profiles",
-        help="Not generating profiles (wgmlst.tsv). This option is simply for "
-             "updating database with alleles. [Default: False]",
-        action='store_true',
-        default=False
-    )
-
-    arg_parser.add_argument(
-        "--debug",
-        help="Debug mode.",
-        action='store_true',
-        default=False
-    )
-
-    return arg_parser.parse_args()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
+@click.group(context_settings=CONTEXT_SETTINGS)
 def main():
-    args = parse_args()
+    """Welcome to Bacterial Epidemiology NGs Analysis (BENGA) pipeline."""
 
-    input_dir = args.input
-    output_dir = args.output
-    database = args.database
-    occr_level = args.occr
-    drop_by_occur = args.drop_by_occur
-    threads = args.threads
-    debug = args.debug
-    new_alleles = not args.no_new_alleles
-    generate_profiles = not args.no_profiles
 
-    if args.algorithm == "make_db":
-        databases.annotate_configs(input_dir, output_dir, threads=threads)
-        database = databases.make_database(output_dir, drop_by_occur, threads=threads)
-        statistics.calculate_loci_coverage(output_dir, output_dir, database=database)
-        statistics.calculate_allele_length(output_dir, database=database)
-    if args.algorithm == "statistics":
-        statistics.calculate_loci_coverage(input_dir, output_dir, database=database)
-        statistics.calculate_allele_length(output_dir, database=database)
-    if args.algorithm == "profiling":
-        profiling.profiling(output_dir, input_dir, database, threads=threads, occr_level=occr_level,
-                            enable_adding_new_alleles=new_alleles, generate_profiles=generate_profiles,
-                            debug=debug)
-    if args.algorithm == "tree":
-        dendro = phylogeny.Dendrogram()
-        dendro.make_tree(os.path.join(input_dir, "profile.tsv"))
-        date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-        filename = date + "_tree"
-        dendro.to_newick(os.path.join(output_dir, "{}.newick".format(filename)))
-        dendro.scipy_tree(os.path.join(output_dir, "{}.pdf".format(filename)))
-        dendro.scipy_tree(os.path.join(output_dir, "{}.svg".format(filename)))
-        dendro.scipy_tree(os.path.join(output_dir, "{}.png".format(filename)))
-        subprocess.call(['libreoffice', '--headless', '--convert-to', 'emf',  '--outdir', output_dir,
-                         os.path.join(output_dir, "{}.svg".format(filename))])
+@main.command("makedb", short_help="Make pan-genome allele database",
+              context_settings=CONTEXT_SETTINGS)
+@click.option('--drop_by_occur', default=0.0, metavar="<float>", type=float,
+              help="Level of occurrence to drop.")
+@click.option('-t', '--threads', default=8, metavar="<int>", type=int,
+              help="Number of threads for computation. [Default: 8]")
+@click.argument('input_dir', type=click.Path(exists=True))
+@click.argument('output_dir', type=click.Path(exists=True))
+def makedb(input_dir, output_dir, drop_by_occur, threads):
+    """Make database with fasta files in INPUT_DIR and output accessory results in OUTPUT_DIR."""
+    databases.annotate_configs(input_dir, output_dir, threads=threads)
+    database = databases.make_database(output_dir, drop_by_occur, threads=threads)
+    statistics.calculate_loci_coverage(output_dir, output_dir, database=database)
+    statistics.calculate_allele_length(output_dir, database=database)
+
+
+@main.command("stats", short_help="Make database statistics",
+              context_settings=CONTEXT_SETTINGS)
+@click.argument('database', type=str)
+@click.argument('input_dir', type=click.Path(exists=True))
+@click.argument('output_dir', type=click.Path(exists=True))
+def statistics(input_dir, output_dir, database):
+    """Plot statistics of DATABASE and profile from INPUT_DIR, and then output to OUTPUT_DIR."""
+    statistics.calculate_loci_coverage(input_dir, output_dir, database=database)
+    statistics.calculate_allele_length(output_dir, database=database)
+
+
+@main.command("profile", short_help="Make profiles against database",
+              context_settings=CONTEXT_SETTINGS)
+@click.option('-o', '--occrrence', default=95, metavar="<int>", type=int,
+              help="Level of occurrence for scheme selection. [Default: 95]")
+@click.option('-t', '--threads', default=8, metavar="<int>", type=int,
+              help="Number of threads for computation. [Default: 8]")
+@click.option('--not-extend', default=False, is_flag=True,
+              help="Disable allele extension. [Default: Enable]")
+@click.option('--no-profiles', default=False, is_flag=True,
+              help="Disable generating profiles (profile.tsv). [Default: Enable]")
+@click.option('--debug', default=False, is_flag=True,
+              help="Print additional information.")
+@click.argument('database', type=str)
+@click.argument('input_dir', type=click.Path(exists=True))
+@click.argument('output_dir', type=click.Path(exists=True))
+def profiling(input_dir, output_dir, database, threads, occrrence, not_extend, no_profiles,
+              debug):
+    """Make profiles with fasta files in INPUT_DIR against DATABASE, and then output to OUTPUT_DIR."""
+    # profiling.profiling(output_dir, input_dir, database, threads=threads, occr_level=occrrence,
+    #                     enable_adding_new_alleles=(not not_extend), generate_profiles=(not no_profiles),
+    #                     debug=debug)
+
+
+@main.command("tree", short_help="Plot dendrogram",
+              context_settings=CONTEXT_SETTINGS)
+@click.argument('input_dir', type=click.Path(exists=True))
+@click.argument('output_dir', type=click.Path(exists=True))
+def tree(input_dir, output_dir):
+    """Plot dendrogram with profile.tsv file in INPUT_DIR, and output to OUTPUT_DIR."""
+    dendro = phylogeny.Dendrogram()
+    dendro.make_tree(os.path.join(input_dir, "profile.tsv"))
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+    filename = date + "_tree"
+    dendro.to_newick(os.path.join(output_dir, "{}.newick".format(filename)))
+    dendro.scipy_tree(os.path.join(output_dir, "{}.pdf".format(filename)))
+    dendro.scipy_tree(os.path.join(output_dir, "{}.svg".format(filename)))
+    dendro.scipy_tree(os.path.join(output_dir, "{}.png".format(filename)))
+    subprocess.call(['libreoffice', '--headless', '--convert-to', 'emf', '--outdir', output_dir,
+                     os.path.join(output_dir, "{}.svg".format(filename))])
 
 
 if __name__ == "__main__":
