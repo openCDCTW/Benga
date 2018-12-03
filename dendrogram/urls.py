d5e503f40528e2fbf1bebf1609862d519d2a1032
Yueh-Hua Tu <a504082002@gmail.com>
2018-12-03 15:36:16 +0800

    Bug fix

--- dendrogram/urls.py
+++ dendrogram/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.BatchList.as_view(), name="upload-list"),
-    path('upload/<uuid:pk>/', views.BatchDetail.as_view(), name="upload-detail"),
     path('profile/', views.ProfileList.as_view(), name="profile-list"),
     path('profile/<uuid:pk>/', views.ProfileDetail.as_view(), name="profile-detail"),
     path('dendrogram/', views.DendrogramList.as_view(), name="dendrogram-list"),
