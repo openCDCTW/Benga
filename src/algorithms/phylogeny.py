0727cd5ff6fac05d3de5c228ce72338eb68752eb
Yueh-Hua Tu <a504082002@gmail.com>
2019-01-18 11:30:49 +0800

    Refactoring

--- src/algorithms/phylogeny.py
+++ src/algorithms/phylogeny.py
@@ -1,6 +1,5 @@
 import fastcluster
 import pandas as pd
-from ete3 import Tree
 from scipy.cluster import hierarchy
 from scipy.spatial.distance import squareform
 import matplotlib
@@ -10,29 +9,49 @@ from collections import Counter
 from matplotlib.ticker import MaxNLocator
 
 
+class DistanceMatrix:
+
+    def __init__(self, profile):
+        profile = profile.fillna('0')
+        profile = profile.transpose()
+        self._profile_array = profile.values
+        self._profile_index = list(profile.index)
+        self._distances = None
+
+    @property
+    def index(self):
+        return self._profile_index
+
+    @property
+    def distance(self):
+        if not self._distances:
+            pairs = []
+            for i in range(len(self._profile_index)):
+                data = (self._profile_index[i], self._profile_index[i], 0)
+                pairs.append(data)
+                for j in range(i+1, len(self._profile_index)):
+                    dist = hamming(self._profile_array[i], self._profile_array[j])
+                    data = (self._profile_index[i], self._profile_index[j], dist)
+                    pairs.append(data)
+            self._distances = pd.DataFrame()
+            for pair in pairs:
+                self._distances.loc[pair[0], pair[1]] = self._distances.loc[pair[1], pair[0]] = pair[2]
+        return self._distances
+
+
 class Dendrogram:
 
-    def __init__(self, tree=None):
-        self._nodes = None
-        self._tree = tree
+    def __init__(self, dm):
+        self._nodes = list(dm.index)
+        self._tree = hierarchy.to_tree(self._linkage, False)
         self._newick = None
-        self._ete_tree = None
-        self._linkage = None
+        self._linkage = fastcluster.single(squareform(dm.distance))
 
     @property
     def newick(self):
         if not self._newick:
             self._newick = make_newick(self._tree, "", self._tree.dist, self._nodes)
         return self._newick
-
-    @property
-    def ete_tree(self):
-        if not self._ete_tree:
-            self._ete_tree = Tree(self.newick)
-        return self._ete_tree
-
-    def render_on(self, file, w=900, h=1200, units="px", dpi=300, *args):
-        self.ete_tree.render(file, w=w, h=h, units=units, dpi=dpi, *args)
 
     def scipy_tree(self, file, distance_annotate=True, w=8, dpi=300):
         plt.style.use("ggplot")
@@ -51,42 +70,13 @@ class Dendrogram:
                              va='top', ha='right', fontsize=8)
         fig.savefig(file, dpi=dpi, bbox_inches='tight', pad_inches=1)
 
-    def make_tree(self, profiles):
-        self._nodes = list(profiles.columns)
-        distances = distance_matrix(profiles)
-        self._linkage = fastcluster.single(squareform(distances))
-        self._tree = hierarchy.to_tree(self._linkage, False)
-
     def to_newick(self, file):
         with open(file, "w") as file:
             file.write(self.newick)
 
 
-def hamming(xs, ys):
-    return sum(xs.ne(ys) & ~(xs.isnull() & ys.isnull()))
-
-
-def distance(array_1, array_2, name_1, name_2):
-    genome_diff = Counter(array_1 == array_2)[False]
-    return name_1, name_2, genome_diff
-
-
-def distance_matrix(profile):
-    profile = profile.fillna('0')
-    profile = profile.transpose()
-    profile_array = profile.values
-    profile_index = [i for i in range(len(profile.index))]
-    pairs = []
-    for i in range(len(profile_index)):
-        index_1 = profile_index.pop(0)
-        pairs.append((profile.index[index_1], profile.index[index_1], 0))
-        for index_2 in profile_index:
-            dist = distance(profile_array[index_1], profile_array[index_2], profile.index[index_1], profile.index[index_2])
-            pairs.append(dist)
-    distances = pd.DataFrame()
-    for pair in pairs:
-        distances.loc[pair[0], pair[1]] = distances.loc[pair[1], pair[0]] = pair[2]
-    return distances
+def hamming(array_1, array_2):
+    return Counter(array_1 == array_2)[False]
 
 
 def make_newick(node, newick, parentdist, leaf_names):
