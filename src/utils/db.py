1964f7a19749c2184276147b8f4ecb11c06ebdf3
Yueh-Hua Tu <a504082002@gmail.com>
2019-04-03 10:02:48 +0800

    Provide table name interface and deal with locus name bug

--- src/utils/db.py
+++ src/utils/db.py
@@ -7,6 +7,29 @@ from sqlalchemy.dialects import postgresql
 from django.conf import settings
 
 DBCONFIG = {}
+METADATA = MetaData()
+LOCI = Table("loci", METADATA,
+             Column("locus_id", None, ForeignKey("locus_meta.locus_id", ondelete="CASCADE"),
+                    primary_key=True),
+             Column("ref_allele", None, ForeignKey("alleles.allele_id", ondelete="CASCADE"),
+                    nullable=False),
+             Column("occurrence", postgresql.REAL))
+PAIRS = Table("pairs", METADATA,
+              Column("allele_id", None, ForeignKey("alleles.allele_id", ondelete="CASCADE"),
+                     primary_key=True, nullable=False),
+              Column("locus_id", None, ForeignKey("locus_meta.locus_id", ondelete="CASCADE"),
+                     primary_key=True))
+ALLELES = Table("alleles", METADATA,
+                Column("allele_id", postgresql.CHAR(64), primary_key=True, nullable=False, unique=True),
+                Column("dna_seq", postgresql.TEXT, nullable=False),
+                Column("peptide_seq", postgresql.TEXT, nullable=False),
+                Column("count", postgresql.SMALLINT, nullable=False))
+LOCUS_META = Table("locus_meta", METADATA,
+                   Column("locus_id", postgresql.VARCHAR(50), primary_key=True, nullable=False),
+                   Column("num_isolates", postgresql.SMALLINT),
+                   Column("num_sequences", postgresql.SMALLINT),
+                   Column("description", postgresql.TEXT),
+                   Column("is_paralog", postgresql.BOOLEAN))
 
 
 def load_database_config(logger=None):
@@ -61,28 +84,5 @@ def create_pgadb_relations(dbname):
     global DBCONFIG
     DBCONFIG["database"] = dbname
     engine = create_engine(URL(**DBCONFIG))
-    metadata = MetaData()
-    loci = Table("loci", metadata,
-                 Column("locus_id", None, ForeignKey("locus_meta.locus_id", ondelete="CASCADE"),
-                        primary_key=True),
-                 Column("ref_allele", None, ForeignKey("alleles.allele_id", ondelete="CASCADE"),
-                        nullable=False),
-                 Column("occurrence", postgresql.REAL))
-    pairs = Table("pairs", metadata,
-                  Column("allele_id", None, ForeignKey("alleles.allele_id", ondelete="CASCADE"),
-                         primary_key=True, nullable=False),
-                  Column("locus_id", None, ForeignKey("locus_meta.locus_id", ondelete="CASCADE"),
-                         primary_key=True))
-    alleles = Table("alleles", metadata,
-                    Column("allele_id", postgresql.CHAR(64), primary_key=True, nullable=False, unique=True),
-                    Column("dna_seq", postgresql.TEXT, nullable=False),
-                    Column("peptide_seq", postgresql.TEXT, nullable=False),
-                    Column("count", postgresql.SMALLINT, nullable=False))
-    locus_meta = Table("locus_meta", metadata,
-                       Column("locus_id", postgresql.VARCHAR(50), primary_key=True, nullable=False),
-                       Column("num_isolates", postgresql.SMALLINT),
-                       Column("num_sequences", postgresql.SMALLINT),
-                       Column("description", postgresql.TEXT),
-                       Column("is_paralog", postgresql.BOOLEAN))
-    metadata.create_all(engine)
+    METADATA.create_all(engine)
     engine.dispose()
