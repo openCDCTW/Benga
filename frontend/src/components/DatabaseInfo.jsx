3d0f4c9670e4ff3719e6177be322c6e62e705a70
y.s.chen <y.s.chen@cdc.gov.tw>
2019-05-03 16:52:05 +0800

    Modify About

--- frontend/src/components/DatabaseInfo.jsx
+++ frontend/src/components/DatabaseInfo.jsx
@@ -15,15 +15,70 @@ export default class DatabaseInfo extends React.Component {
 		if(this.props.database == 1){
 			return (
 				<div>
-					<br />
 					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
-					margin:'auto',fontSize:16, marginTop: '60px' }}>
-					Core gene: 2951
+					margin:'auto',fontSize:16 }}>
+						&nbsp;&nbsp;&nbsp;&nbsp;cgMLST@Taiwan provides allele database, cgMLST database, 
+						and tools for cgMLST profiling of whole genome sequences, strain tracking, 
+						and clustering of cgMLST profiles of Vibrio cholerae via the internet. 
+						cgMLST profiling is based on 2,951 V. cholerae core genes, 
+						which are identified from 1,647 V. 
+						cholerae genomes from the NCBI database. 
+						Core genes are designated for those existing in more than 95% of the 1,647 genomes. 
 					</Typography>
+					<br />
 					<div style={{ display:'flex', justifyContent:'center', alignItems:'center'}}>
 	                	<img style={{ width:'95%'}} 
-	                	src={require('./static/Vibrio_cholerae_locus_distribution.png')} />
+	                	src={require('./static/VC_loci_feq_distribution.png')} />
 	            	</div>
+	            	<br />
+	            	<Typography component="p" style={{ width:'90%',textAlign: 'center',
+					margin:'auto',fontSize:16 }}>
+						Figure. Frequency of loci (genes) over 1,647 V. cholerae genomes.
+					</Typography>
+					<br />
+					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
+					margin:'auto',fontSize:16 }}>
+						&nbsp;&nbsp;&nbsp;&nbsp;cgMLST database contains cgMLST profiles for 
+						the strains with genomic sequences deposited in the NCBI database. 
+						Nowadays, the database contains 5,048 cgMLST profiles and will be updated 
+						by time.
+					</Typography>
+					<br />
+					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
+					margin:'auto',fontSize:16 }}>
+						&nbsp;&nbsp;&nbsp;&nbsp;cgMLST@Taiwan was developed in the laboratories 
+						of Centers for Disease Control, Ministry of Health and Welfare, 
+						Taiwan by Yueh-Hua Tu, Yi-Syong Chen, Bo-Han Chen, Yen-Yi Liu, Yu-Ping Hong, 
+						Ru-Hsiou Teng, You-Wun Wang, and Chien-Shun Chiou.
+					</Typography>
+					<br />
+					<h3 style={{ marginLeft:'30px' }}>&nbsp;CITATIONS</h3>
+					<br />
+					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
+					margin:'auto',fontSize:16 }}>
+						For publication of results, please cite: 
+					</Typography>
+					<br />
+					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
+					margin:'auto',fontSize:16 }}>
+						Yueh-Hua Tu, Yi-Syong Chen, Bo-Han Chen, Yen-Yi Liu, Yu-Ping Hong, 
+						Ru-Hsiou Teng, You-Wun Wang, and Chien-Shun Chiou. cgMLST@Taiwan: 
+						A web service for Vibrio cholerae cgMLST profiling and global strain tracking. 
+						J Clin Microbiol. 2019.
+					</Typography>
+					<br />
+					<h3 style={{ marginLeft:'30px' }}>&nbsp;Contact</h3>
+					<Typography component="p" style={{ width:'90%',
+					margin:'auto',fontSize:16 }}>
+						For any question please contact Chien-Shun Chiou by email:
+					</Typography>
+					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
+					margin:'auto',fontSize:16 }}>
+						<font style={{ color:'blue', textDecoration:'underline' }}>nipmcsc@cdc.gov.tw</font>
+						 &nbsp; or &nbsp;
+						 <font style={{ color:'blue', textDecoration:'underline' }}>nipmcsc@gmail.com</font>
+					</Typography>
+					<br />
 				</div>
 			)
 		}
