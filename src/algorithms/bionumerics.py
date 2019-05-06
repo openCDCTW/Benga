c16028f008dc592a2a67ba9111b807df2db58f91
chen1i6c04 <chen1i6c04@gmail.com>
2019-05-06 13:54:32 +0800

    speed up profile convert to bionumerics format

--- src/algorithms/bionumerics.py
+++ src/algorithms/bionumerics.py
@@ -1,19 +1,17 @@
-def generate_encoder(alleles):
-    encoder = {}
-    for i, x in enumerate(sorted(set(alleles)), 1):
-        if x == '0':
-            encoder[x] = 0
-        else:
-            encoder[x] = i
-    return encoder
+import pandas as pd
 
 
 def to_bionumerics_format(profile):
-    wgmlst = profile.fillna('0').transpose()
-    for col in wgmlst.columns:
-        alleles = wgmlst[col]
-        encoder = generate_encoder(alleles)
-        wgmlst[col] = [encoder[i] for i in alleles]
-    wgmlst.insert(0, 'Key', wgmlst.index)
-    return wgmlst
-
+    profile = profile.fillna(0)
+    profile_values = profile.values
+    new_values = []
+    for value in profile_values:
+        allele_set = set(value)
+        allele_set.discard(0)
+        encoder = {allele: index for index, allele in enumerate(allele_set, 1)}
+        value = [encoder.get(i, 0) for i in value]
+        new_values.append(value)
+    df = pd.DataFrame(data=new_values, index=profile.index, columns=profile.columns)
+    df = df.transpose()
+    df.insert(0, 'Key', df.index)
+    return df
