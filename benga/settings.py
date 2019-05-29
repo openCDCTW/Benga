6b5cdd916fbe673b302c37522c15b450fb29179a
Yueh-Hua Tu <a504082002@gmail.com>
2019-05-29 15:09:28 +0800

    Bug fix

--- benga/settings.py
+++ benga/settings.py
@@ -81,18 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'benga.wsgi.application'
 
-# Channels
-ASGI_APPLICATION = 'benga.routing.application'
-
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,23 +5,33 @@ git clone https://github.com/openCDCTW/Benga.git
 cd Benga/
 virtualenv venv -p python3
 . venv/bin/activate
-pip install --trusted-host pypi.python.org -r requirements.txt --upgrade
+pip install -r requirements.txt --upgrade
+
 #front-end
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 ##restart
-exit
-ssh -i /home/yschen/cdc/cdc-virginia.pem ubuntu@3.87.10.56
+# disconnect and reconnect
 cd Benga/
 . venv/bin/activate
+
 ##
 nvm install v10.13.0
 npm install
 export NODE_OPTIONS=--max_old_space_size=4096
 npm run build
+# modify environment variables
 . scripts/run_envs.sh  envs/web.env 
 . scripts/generate_secret_key.sh
-#nginx
-pip install uwsgi
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+
+sudo chown -R www-data /var/www/benga
+sudo chmod -R 755 /var/www/benga
+sudo systemctl reload nginx.service
+sudo systemctl restart benga.service
