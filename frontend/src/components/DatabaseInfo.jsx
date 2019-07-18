c808a4d6b418fe780d24687a84273c0a8ffce5ba
y.s.chen <y.s.chen@cdc.gov.tw>
2019-07-18 15:13:59 +0800

    Remove tutorial and modify DatabaseInfo

--- frontend/src/components/DatabaseInfo.jsx
+++ frontend/src/components/DatabaseInfo.jsx
@@ -17,18 +17,18 @@ export default class DatabaseInfo extends React.Component {
 				<div>
 					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
 					margin:'auto',fontSize:16 }}>
-						&nbsp;&nbsp;&nbsp;&nbsp;cgMLST@Taiwan provides allele database, cgMLST database, 
-						and tools for cgMLST profiling of whole genome sequences, strain tracking, 
-						and clustering of cgMLST profiles of 
-						<font style={{ fontStyle:'italic' }}> Vibrio cholerae </font>
-						via the internet. 
-						cgMLST profiling is based on 2,951
+						cgMLST@Taiwan provides a 
+						<font style={{ fontStyle:'italic' }}> Vibrio cholerae </font> 
+						allele database,
 						<font style={{ fontStyle:'italic' }}> V. cholerae </font>
-						core genes, 
-						which are identified from 1,647
-						<font style={{ fontStyle:'italic' }}> V. cholerae </font>
-						genomes from the NCBI database. 
-						Core genes are designated for those existing in more than 95% of the 1,647 genomes. 
+						cgMLST profile database, and tools for cgMLST profiling, strain tracking, 
+						and clustering of cgMLST profiles via the internet. cgMLST profiling 
+						is based on 2,951 
+						<font style={{ fontStyle:'italic' }}> V. cholerae </font> 
+						core genes, which are identified from 1,647
+						<font style={{ fontStyle:'italic' }}> V. cholerae </font> 
+						genomes from the NCBI database. Core genes are designated 
+						or those existing in more than 95% of the 1,647 genomes.
 					</Typography>
 					<br />
 					<div style={{ display:'flex', justifyContent:'center', alignItems:'center'}}>
@@ -45,21 +45,21 @@ export default class DatabaseInfo extends React.Component {
 					<br />
 					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
 					margin:'auto',fontSize:16 }}>
-						&nbsp;&nbsp;&nbsp;&nbsp;cgMLST database contains cgMLST profiles for 
-						the strains with genomic sequences deposited in the NCBI database. 
+						The cgMLST profile database contains cgMLST profiles for the 
+						<font style={{ fontStyle:'italic' }}> V. cholerae </font> 
+						strains with genomic sequences deposited in the NCBI database. 
 						Nowadays, the database contains 5,048 cgMLST profiles and will be updated 
 						by time.
 					</Typography>
 					<br />
 					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
 					margin:'auto',fontSize:16 }}>
-						&nbsp;&nbsp;&nbsp;&nbsp;cgMLST@Taiwan was developed in the laboratories 
-						of Centers for Disease Control, Ministry of Health and Welfare, 
-						Taiwan by Yueh-Hua Tu, Yi-Syong Chen, Bo-Han Chen, Yen-Yi Liu, Yu-Ping Hong, 
-						Ru-Hsiou Teng, You-Wun Wang, and Chien-Shun Chiou.
+						cgMLST@Taiwan was developed in the laboratories of Centers for Disease Control, 
+						Ministry of Health and Welfare, Taiwan by Yueh-Hua Tu, Yi-Syong Chen, Bo-Han Chen, 
+						Yen-Yi Liu, Yu-Ping Hong, Ru-Hsiou Teng, You-Wun Wang, and Chien-Shun Chiou.
 					</Typography>
 					<br />
-					<h3 style={{ marginLeft:'30px' }}>&nbsp;CITATIONS</h3>
+					<h3 style={{ marginLeft:'30px' }}>CITATIONS</h3>
 					<br />
 					<Typography component="p" style={{ width:'90%',textAlign: 'justify',
 					margin:'auto',fontSize:16 }}>
@@ -76,7 +76,7 @@ export default class DatabaseInfo extends React.Component {
 						J Clin Microbiol. 2019.
 					</Typography>
 					<br />
-					<h3 style={{ marginLeft:'30px' }}>&nbsp;Contact</h3>
+					<h3 style={{ marginLeft:'30px' }}>Contact</h3>
 					<Typography component="p" style={{ width:'90%',
 					margin:'auto',fontSize:16 }}>
 						For any question please contact Chien-Shun Chiou by email:
