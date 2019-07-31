e1b96370b41846033e0299ee19f66a5ccd6feff4
chen1i6c04 <chen1i6c04@gmail.com>
2019-07-31 10:00:10 +0800

    Update src/algorithms/bionumerics.py
    Co-Authored-By: Yueh-Hua Tu <a504082002@gmail.com>

--- src/algorithms/bionumerics.py
+++ src/algorithms/bionumerics.py
@@ -2,6 +2,6 @@ from ..utils.data import convert_data_type
 
 
 def to_bionumerics_format(profile):
-    profile = convert_data_type(profile).T
+    profile = integer_encoding(profile).T
     profile.insert(0, 'Key', profile.index)
     return profile
