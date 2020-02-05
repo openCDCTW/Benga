83995cbf2b4022ef64608438da1c243b987be382
chen1i6c04 <chen1i6c04@gmail.com>
2020-02-05 13:29:03 +0800

    add metadata columns list to nosql database

--- src/utils/nosql.py
+++ src/utils/nosql.py
@@ -5,6 +5,21 @@ from pymongo import MongoClient
 NOSQLCONFIG = {}
 
 
+class NoSQL:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benga.settings")
+
+    def __init__(self, logger=None):
+        self.config = dict()
+        self.config["host"] = settings.NOSQLS['mongodb']['HOST']
+        self.config["port"] = int(settings.NOSQLS['mongodb']['PORT'])
+        logger.info("Database: {}:{}".format(self.config["host"], self.config["port"]))
+
+    def connect(self, database):
+        client = MongoClient(self.config["host"], self.config["port"])
+        db = client[database]
+        return db['track_data'], db['metadata_cols']
+
+
 def load_database_config(logger=None):
     global NOSQLCONFIG
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benga.settings")
@@ -20,4 +35,4 @@ def get_dbtrack(database):
     NOSQLCONFIG["port"] = int(settings.NOSQLS['mongodb']['PORT'])
     client = MongoClient(NOSQLCONFIG["host"], NOSQLCONFIG["port"])
     db = client[database]
-    return db.track
+    return db['track_data'], db['metadata_cols']
