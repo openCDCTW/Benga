336cf5a95f5e4859c07c48c7d5e11b7f9252ee24
chen1i6c04 <chen1i6c04@gmail.com>
2020-10-27 14:21:40 +0800

    modify structure of tracking database

--- src/utils/nosql.py
+++ src/utils/nosql.py
@@ -8,31 +8,29 @@ NOSQLCONFIG = {}
 class NoSQL:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benga.settings")
 
-    def __init__(self, logger=None):
-        self.config = dict()
-        self.config["host"] = settings.NOSQLS['mongodb']['HOST']
-        self.config["port"] = int(settings.NOSQLS['mongodb']['PORT'])
-        logger.info("Database: {}:{}".format(self.config["host"], self.config["port"]))
+    def __init__(self, database_name):
+        global NOSQLCONFIG
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benga.settings")
+        NOSQLCONFIG["host"] = settings.NOSQLS['mongodb']['HOST']
+        NOSQLCONFIG["port"] = int(settings.NOSQLS['mongodb']['PORT'])
+        self.client = MongoClient(NOSQLCONFIG["host"], NOSQLCONFIG["port"])
+        self.database = self.client[database_name]
 
-    def connect(self, database):
-        client = MongoClient(self.config["host"], self.config["port"])
-        db = client[database]
-        return db['track_data'], db['metadata_cols']
+    def disconnect(self):
+        self.client.close()
 
+    def profilesIterator(self):
+        collection = self.database['Sample_attribute']
+        return collection.find({}, {'profile': 1, 'NCBIAccession': 1})
 
-def load_database_config(logger=None):
-    global NOSQLCONFIG
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benga.settings")
-    NOSQLCONFIG["host"] = settings.NOSQLS['mongodb']['HOST']
-    NOSQLCONFIG["port"] = int(settings.NOSQLS['mongodb']['PORT'])
-    logger.info("Database: {}:{}".format(NOSQLCONFIG["host"], NOSQLCONFIG["port"]))
+    @property
+    def fields(self):
+        return self.database.Description.find_one({'fields': {'$exists': True}})['fields']
 
+    @property
+    def core_genome(self):
+        return self.database.Description.find_one({'core_genome': {'$exists': True}})['core_genome']
 
-def get_dbtrack(database):
-    global NOSQLCONFIG
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "benga.settings")
-    NOSQLCONFIG["host"] = settings.NOSQLS['mongodb']['HOST']
-    NOSQLCONFIG["port"] = int(settings.NOSQLS['mongodb']['PORT'])
-    client = MongoClient(NOSQLCONFIG["host"], NOSQLCONFIG["port"])
-    db = client[database]
-    return db['track_data'], db['metadata_cols']
+    def fetch_attrib(self, filter_, skip):
+        return self.database.Sample_attribute.find(filter_, skip)
+
